cd7b8be8bdf8b569c75ec37b192c050b139981ea
student <student@devin-milberg.dalton.org>
2018-10-16 10:41:10 -0400

    commit2

--- Array2/src/ArrayTestGit.java
+++ Array2/src/ArrayTestGit.java
@@ -1,8 +1,15 @@
-
 public class ArrayTestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayGit1 aTest = new ArrayGit1(1987, 600);
+		
+		aTest.addNode(1979, 650);
+		aTest.addNode(1985, 630);
+		
+		
+		System.out.println(aTest.searchYr(1979).year + " " + aTest.searchYr(1979).water);
+		
+		System.out.println(aTest.searchW(1985).year + " " + aTest.searchW(1985).water);
 
 	}
 
