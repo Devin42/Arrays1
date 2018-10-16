686759212f6e10b0ea7672eb90bc4485fe3d0a35
student <student@devin-milberg.dalton.org>
2018-10-16 10:47:07 -0400

    re-commit

---
+++ Array2/src/package1/ArrayTestGit.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class ArrayTestGit {
+
+	public static void main(String[] args) {
+		ArrayGit1 aTest = new ArrayGit1(1987, 600);
+		
+		aTest.addNode(1979, 650);
+		aTest.addNode(1985, 630);
+		
+		
+		System.out.println(aTest.searchYr(1979).year + " " + aTest.searchYr(1979).water);
+		
+		System.out.println(aTest.searchW(1985).year + " " + aTest.searchW(1985).water);
+
+	}
+
+}
---
+++ Array2/src/package1/Node.java
@@ -0,0 +1,24 @@
+package package1;
+
+
+public class Node {
+
+	//comment
+	int year;
+	double water;
+	Node n1Yr;
+	Node n2Yr;
+	Node n1W;
+	Node n2W;
+	
+	public Node(int year, double water, Node n1Yr, Node n2Yr, Node n1W, Node n2W){
+		this.year = year;
+		this.water = water;
+		this.n1Yr = n1Yr;
+		this.n2Yr = n2Yr;
+		this.n2W = n1W;
+		this.n2W = n2W;
+	}
+
+	
+}
